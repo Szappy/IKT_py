f5c93190f2f197400672b2e9ca4dc899c644a1a8
Bosnyák Zsuzsanna Dóra <bosnyak.zsuzsanna@digi.hu>
2025-01-06 12:46:56 +0100

    Belépés éve szerinti lista

--- kereses.py
+++ kereses.py
@@ -1,4 +1,4 @@
-from file import szotar_lista
+from lista import *
 
 # Elsődleges kulcs szerinti keresés
 def pri_keres (nev, szev):
@@ -8,3 +8,16 @@ def pri_keres (nev, szev):
         if szotar_lista[i]["NEV"].casefold() == search_nev.casefold() and szotar_lista[i]["SZULETESI_EV"] == search_szev:
             return i
     return None
+
+    # Belépés éve szerinti lista
+def belep_list ():
+    fej = False
+    belepes_eve = int(input("Adja meg a listázandó évet: "))
+    for i in range(len(szotar_lista)):
+        if szotar_lista[i]["START_EV"] == belepes_eve:
+            if fej == False:
+                fej_iras()
+                fej = True
+            egy_sor_iras(szotar_lista[i],i)
+    if fej == False:
+        print("Nincs ilyen évvel kapcsolatos adat.")
